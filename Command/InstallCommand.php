bd89e12a78d5e1bae59ffe40d6bfae8411a8bf50
Umberto Stefani <umbe81@gmail.com>
2016-03-23 14:19:54 +0100

    Fixed installation procedure: fixed sql-dump option and refactoring of method for schema table creation

--- Command/InstallCommand.php
+++ Command/InstallCommand.php
@@ -4,13 +4,11 @@
 namespace BlueSteel42\SettingsBundle\Command;
 
 use Doctrine\DBAL\Connection;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
-use Symfony\Component\Yaml\Exception\ParseException;
-use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Form\Exception\RuntimeException;
 
 
 class InstallCommand extends AbstractInstallCommand
@@ -26,7 +24,7 @@ class InstallCommand extends AbstractInstallCommand
             ->setHelp(<<<EOT
 The <info>%command.name%</info> prepare configuration environment to starting to use SettingsBundle.
 The <comment>connection</comment> parameter will set database connection to use.
-The <comment>table_name</comment> parameter will set table name.
+The <comment>table_name</comment> parameter will set table name. If table already exixts, will be dropped and re-created.
 
 Example 1 - With arguments:
 
@@ -72,71 +70,74 @@ EOT
             $tbl = $argTable;
         }
 
-        $yml = new Parser();
-        $cfg = null;
-        try {
-            $cfg = $yml->parse(file_get_contents(__DIR__ . '/../../../../app/config/config.yml'));
-        } catch (ParseException $e) {
-            printf("Unable to parse the YAML string: %s", $e->getMessage());
+        $conQuestion = new Question(sprintf('<question>Please select Doctrine Connection: [%s]</question>', $con), $con);
+        $tblQuestion = new Question(sprintf('<question>Please select Table Name: [%s]</question>', $tbl), $tbl);
+        $conAnswer = ($useQuestionCon) ? $this->getHelper('question')->ask($input, $output, $conQuestion) : $argConnection;
+        $tblAnswer = ($useQuestionTable) ? $this->getHelper('question')->ask($input, $output, $tblQuestion) : $argTable;
+
+        if (!array_key_exists($conAnswer, $connections)) {
+            throw new \InvalidArgumentException(sprintf("Connection '%s' does not exist", $conAnswer));
         }
 
-        if (array_key_exists('bluesteel42_settings', $cfg)) {
+        /** @var Connection $conn */
+        $conn = $this->get($connections[$conAnswer]);
+        $sm = $conn->getSchemaManager();
+        $tableExists = $sm->tablesExist($tblAnswer);
 
-//            if ($cfg['bluesteel42_settings']['backend'] == 'yml' || $cfg['bluesteel42_settings']['backend'] == 'xml') {
-//                $ext = $cfg['bluesteel42_settings']['backend'];
-//                $locator = new FileLocator($this->getContainer()->getParameter(sprintf('bluesteel42.settings.%s.path', $ext)));
-//                $cfgFile = $locator->locate(sprintf('bluesteel42_settings.%s', $ext), $this->getContainer()->getParameter(sprintf('bluesteel42.settings.%s.path', $ext), false));
-//            }
+        $fromSchema = $sm->createSchema();
+        $toSchema = clone $fromSchema;
 
-            $conQuestion = new Question(sprintf('<question>Please select Doctrine Connection: [%s]</question>', $con), $con);
-            $tblQuestion = new Question(sprintf('<question>Please select Table Name: [%s]</question>', $tbl), $tbl);
-            $conAnswer = ($useQuestionCon) ? $this->getHelper('question')->ask($input, $output, $conQuestion) : $argConnection;
-            $tblAnswer = ($useQuestionTable) ? $this->getHelper('question')->ask($input, $output, $tblQuestion) : $argTable;
-
-            if (!array_key_exists($conAnswer, $connections)) {
-                throw new \InvalidArgumentException(sprintf('The connection %s does not exist', $conAnswer));
-            }
-            /** @var Connection $conn */
-            $conn = $this->get($connections[$conAnswer]);
-            $sm = $conn->getSchemaManager();
-            $tableExists = $sm->tablesExist($tblAnswer);
-
+        if (!$tableExists) {
+            //  Table not Exists
+            $this->createTableByToSchema($toSchema, $tblAnswer);
+            $sqlQueryList = $fromSchema->getMigrateToSql($toSchema, $conn->getDatabasePlatform());
             $conn->beginTransaction();
-            if (!$tableExists) {
-                try {
-                    $fromSchema = $sm->createSchema();
-                    $toSchema = clone $fromSchema;
-                    $myTable = $toSchema->createTable($tblAnswer);
-                    $myTable->addColumn("id", "string", array("customSchemaOptions" => array("unique" => true)));
-                    $myTable->addColumn("val", "text");
-                    $myTable->addColumn("hasChildren", "boolean");
-                    $myTable->setPrimaryKey(array("id"));
-                    $sqlQueryList = $fromSchema->getMigrateToSql($toSchema, $conn->getDatabasePlatform());
-                    foreach ($sqlQueryList as $sql) {
-                        if ($dump) {
-                            $output->writeln($sql);
-                        } else {
-                            $conn->executeQuery($sql);
-                            $conn->commit();
-                            $output->writeln(sprintf("Table %s successfully created.", $tblAnswer));
-                        }
+            try {
+                foreach ($sqlQueryList as $sql) {
+                    if ($dump) {
+                        $output->writeln($sql);
+                    } else {
+                        $conn->executeQuery($sql);
+                        $conn->commit();
+                        $output->writeln(sprintf("Table '%s' successfully created.", $tblAnswer));
                     }
-                } catch (\Exception $e) {
-                    $conn->rollBack();
-                    throw $e;
                 }
-
-            } else {
-                if ($dump) {
-                    $sql = sprintf("CREATE TABLE %s (id VARCHAR(255) NOT NULL UNIQUE, val LONGTEXT NOT NULL, hasChildren TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB", $tblAnswer);
-                    $output->writeln(sprintf("<comment>%s</comment>", $sql));
-                } else {
-                    $conn->rollBack();
-                    throw new \RuntimeException(sprintf("Table %s already exists.", $tblAnswer));
-                }
+            } catch (\Exception $e) {
+                $conn->rollBack();
+                throw $e;
             }
         } else {
-            throw new \RuntimeException("Configuration key bluesteel42_settings not found.");
+            //  Table not Exists
+            $output->writeln(sprintf("Table '%s' already exists.", $tblAnswer));
+            if ($dump) {
+                //  Dump Only output
+                $tmpSuffix = '_bs42_';
+                $this->createTableByToSchema($toSchema, $tblAnswer . $tmpSuffix);
+                $sqlQueryList = $fromSchema->getMigrateToSql($toSchema, $conn->getDatabasePlatform());
+                foreach ($sqlQueryList as $sql) {
+                    $sql = str_replace($tmpSuffix, '', $sql);
+                    $output->writeln($sql);
+                }
+            } else {
+                throw new RuntimeException(sprintf("Table '%s' on connection '%s' already exists.", $tblAnswer, $conAnswer));
+            }
+
         }
+
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\ $toSchema
+     * @param string $tblAnswer
+     * @return \Doctrine\DBAL\Schema\Table $myTable
+     */
+    protected function createTableByToSchema($toSchema, $tblAnswer)
+    {
+        $myTable = $toSchema->createTable($tblAnswer);
+        $myTable->addColumn("id", "string", array("customSchemaOptions" => array("unique" => true)));
+        $myTable->addColumn("val", "text");
+        $myTable->addColumn("hasChildren", "boolean");
+        $myTable->setPrimaryKey(array("id"));
+        return $myTable;
     }
 }
